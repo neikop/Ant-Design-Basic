664b6f8b9f49000693f1b91162c85b277e666288
Khắc Kiên <kien.nk@teko.vn>
2020-05-28 09:54:52 +0700

    feature 2

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 1
+            Learn React
           </a>
         </header>
       </div>
